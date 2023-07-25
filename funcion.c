302d6f343bc5e70b3ee997b06ddfbb9f592bd709
Marco <maphy1707@gmail.com>
2023-07-24 22:18:13 -0600

    Ejemplo de funcion

---
+++ funcion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void saludo(){
+	
+	printf("Hola.");
+	return;
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+	saludo();
+	
+	return 0;
+}
